ebcf1c16e22e225d5076036678e0a9ba208e5cf1
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-23 17:52:17 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -9,10 +9,7 @@ def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
   
-  count = 0
-  while count < show.length do
-    BASE_HOA[show] << name
-    count += 1
-  end
+  
+  BASE_HOA[show] << name
   BASE_HOA
 end
