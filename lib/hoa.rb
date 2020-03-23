9e03fca234153988bc091b9a59adf1416c83f4da
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-23 17:56:03 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -9,7 +9,6 @@ def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
   
-  
   BASE_HOA[show] << name
-  BASE_HOA
+  BASE_HOA[show]
 end
